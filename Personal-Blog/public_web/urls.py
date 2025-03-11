6f02e5c814215868c00d29d684fd1a9c0bab68fc
Francisco Meneses <69993929+LW-Homeless@users.noreply.github.com>
2025-03-11 00:47:35 -0300

    Delete Personal-Blog directory

--- Personal-Blog/public_web/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import index, about, contact, detail_post
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('about', about, name='about'),
-    path('contact', contact, name='contact'),
-    path('detail-post/<int:id>/', detail_post, name='detail_post'),
-]
